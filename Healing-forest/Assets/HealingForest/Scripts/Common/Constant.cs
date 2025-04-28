6387bdc89db022847ac2ce99b56bbc057bc06ff8
aube.lee <leejjoo112@icloud.com>
2025-04-29 00:53:04 +0900

    feat: 공통 constant 추가

---
+++ Healing-forest/Assets/HealingForest/Scripts/Common/Constant.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HF
+{
+    public class Constant
+    {
+
+    }
+
+    // 아이템 카테고리를 위한 enum
+    public enum ItemCategory
+    {
+        Material,   // 재료 아이템
+        Crafting,   // 제작 아이템
+        Equipment   // 장비 아이템
+    }
+
+    public enum ToolType
+    {
+        None = 0,
+        Axe,
+        Shovel,
+        FishingRod,
+        Net,
+
+        End,
+    }
+}
