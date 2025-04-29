0de968668ad56316a72391b8355425e692ed804c
aube.lee <leejjoo112@icloud.com>
2025-04-29 22:55:54 +0900

    feat: item data 구조 개선 작업#1

--- Healing-forest/Assets/HealingForest/Scripts/GameData/ItemDataSO.cs
+++ Healing-forest/Assets/HealingForest/Scripts/GameData/ItemDataSO.cs
@@ -1,20 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace HF
 {
-    // 아이템 카테고리를 위한 enum
-    public enum ItemCategory
-    {
-        Material,   // 재료 아이템
-        Crafting,   // 제작 아이템
-        Equipment   // 장비 아이템
-    }
-
-    // 아이템 기본 데이터 클래스
-    [System.Serializable]
-    public class ItemData
+    [CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData")]
+    public class ItemDataSO : ScriptableObject
     {
         [SerializeField] private string itemID;             // 아이템 고유 ID
         [SerializeField] private string itemName;        // 아이템 이름
@@ -32,18 +21,5 @@ namespace HF
         public float DefaultDurability => defaultDurability;
         public GameObject VisualPrefab => visualPrefab;
         public Sprite Icon => icon;
-
-        // 생성자
-        public ItemData(string id, string name, ItemCategory cat, int stack, float durability, GameObject prefab, Sprite itemIcon)
-        {
-            itemID = id;
-            itemName = name;
-            category = cat;
-            maxStack = stack;
-            defaultDurability = durability;
-            visualPrefab = prefab;
-            icon = itemIcon;
-        }
     }
-
 }
