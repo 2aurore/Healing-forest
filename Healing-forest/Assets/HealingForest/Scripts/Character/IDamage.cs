68e5a797b8a9beb9ca7adba4218664e12354b648
aube.lee <leejjoo112@icloud.com>
2025-06-19 20:06:24 +0900

    feat: damage 인터페이스 추가

---
+++ Healing-forest/Assets/HealingForest/Scripts/Character/IDamage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HF
+{
+    public interface IDamage
+    {
+        public void Damage(CharacterBase attacker);
+    }
+}
