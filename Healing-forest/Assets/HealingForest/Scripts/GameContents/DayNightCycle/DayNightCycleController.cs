d543c486df3190d4f24e75916b1ca108f9d5b0cb
aube.lee <leejjoo112@icloud.com>
2025-06-30 23:11:59 +0900

    feat: 시간의 흐름에 따라 Skybox 제어

--- Healing-forest/Assets/HealingForest/Scripts/GameContents/DayNightCycle/DayNightCycleController.cs
+++ Healing-forest/Assets/HealingForest/Scripts/GameContents/DayNightCycle/DayNightCycleController.cs
@@ -18,9 +18,16 @@ namespace HF
         public Light mainLight;
         public Gradient lightGradient;
 
+        public Material skyboxMaterial; // 원본 Material 파일
+        private Material skyboxInstatnce; // 원본을 복사한 Material 인스턴스
+        public AnimationCurve skyboxBlendCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
         private void Awake()
         {
             Instance = this;
+
+            skyboxInstatnce = Instantiate(skyboxMaterial);
+            RenderSettings.skybox = skyboxInstatnce;
         }
 
         private void Update()
@@ -60,6 +67,12 @@ namespace HF
             // Color - light 의 색상 조절
             Color color = lightGradient.Evaluate(timeOfDay);
             mainLight.color = color;
+
+            // Skybox - skybox material 변경
+            float normalizedBlend = Mathf.Abs(timeOfDay - 0.5f) * 2f; // 0 to 1 for day, 1 to 0 for night
+            //
+            float skyBlend = skyboxBlendCurve.Evaluate(normalizedBlend);
+            skyboxInstatnce.SetFloat("_BlendCubemaps", skyBlend);
         }
     }
 }
