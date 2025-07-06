95d00d1d5e7fca74dfc2d19670d9ad89a349cd38
aube.lee <leejjoo112@icloud.com>
2025-07-06 20:41:45 +0900

    fix: 제작 레시피의 아이템 슬롯 코드 수정

--- Healing-forest/Assets/HealingForest/Scripts/UI/Crafting_Recipt_Material.cs
+++ Healing-forest/Assets/HealingForest/Scripts/UI/Crafting_Recipt_Material.cs
@@ -13,26 +13,43 @@ namespace HF
         [SerializeField] private TextMeshProUGUI amountText;
         [SerializeField] private TextMeshProUGUI requiredCountText;
 
+        private string itemID; // 현재 슬롯의 아이템 ID
+        private int requiredCount; // 필요한 수량
+
+        public string ItemID => itemID;
+
         public void SetMaterialSlot(string itemID, int amount, int requiredCount)
         {
+            this.itemID = itemID;
+            this.requiredCount = requiredCount;
+
             ItemDataSO itemData = GameDataModel.Singleton.GetItemData(itemID);
             if (itemData != null)
             {
                 icon.sprite = itemData.Icon;
                 nameText.text = itemData.ItemName;
-                amountText.text = amount.ToString();
                 requiredCountText.text = requiredCount.ToString();
 
-                if (amount >= requiredCount)
-                {
-                    // 재료가 충분할 때의 처리
-                    amountText.color = Color.white; // 기본 색상으로 설정 (예: 흰색)
-                }
-                else
-                {
-                    // 재료가 부족할 때의 처리
-                    amountText.color = Color.red; // 빨간색으로 변경
-                }
+                UpdateAmount(amount);
+            }
+        }
+
+        /// <summary>
+        /// 보유 수량만 업데이트하는 메서드
+        /// </summary>
+        public void UpdateAmount(int amount)
+        {
+            amountText.text = amount.ToString();
+
+            if (amount >= requiredCount)
+            {
+                // 재료가 충분할 때의 처리
+                amountText.color = Color.white; // 기본 색상으로 설정 (예: 흰색)
+            }
+            else
+            {
+                // 재료가 부족할 때의 처리
+                amountText.color = Color.red; // 빨간색으로 변경
             }
         }
 
