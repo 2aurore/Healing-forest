a9da739765e4c3f0cee38fe74203ccc170938df9
aube.lee <leejjoo112@icloud.com>
2025-08-10 21:53:56 +0900

    fix: player의 home entry 포인트 변경

--- Healing-forest/Assets/HealingForest/Scripts/GameContents/HouseInteract.cs
+++ Healing-forest/Assets/HealingForest/Scripts/GameContents/HouseInteract.cs
@@ -9,7 +9,7 @@ namespace HF
         public void Interact(CharacterBase actor)
         {
 
-            UserDataModel.Singleton.SetCharacterPosition(new Vector3(0, 0, 4));
+            UserDataModel.Singleton.SetCharacterPosition(new Vector3(0, 0, -2));
             LevelLoader.Instance.LoadLevel(LevelType.Home);
             // EventSystem.OnLightToggle(false);   // 라이트를 끄는 이벤트 호출
         }
